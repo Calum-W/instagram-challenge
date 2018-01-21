3fb124a89aa5fae0348d5e14050ca81ce8d9dceb
Calum-W <cal7907@hotmail.co.uk>
2018-01-21 16:54:00 +0000

    Username added to registration. Switches to custom user views from Devise defaults

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root 'posts#index'
   resources :posts
+
+  devise_scope :user do
+    get "/sign_up" => "user/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
